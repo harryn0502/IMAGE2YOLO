d81e208657edd1bfad17f087a0efba0427a96616
Harryn Oh <ohharryn0502@gmail.com>
2024-02-28 17:46:22 +0000

    refactor: Refactor image2yolo script into a class

--- image2yolo.py
+++ image2yolo.py
@@ -3,35 +3,49 @@ from image2coco import Image2Coco
 from coco2yolo import Coco2Yolo
 from shutil import rmtree
 
-if __name__ == '__main__':
+class Image2Yolo:
+    def __init__(self):
+        self.image2coco = Image2Coco()
+        self.coco2yolo = Coco2Yolo()
+
+    def convert(self, data_path, yolo_path, copy_images=False):
+        # Define the paths
+        data_train_path = os.path.join(data_path, 'train')
+        data_valid_path = os.path.join(data_path, 'valid')
+        yolo_train_path = os.path.join(yolo_path, 'train')
+        yolo_valid_path = os.path.join(yolo_path, 'valid')
+        temp_path = os.path.join(yolo_path, 'temp')
+        temp_train_path = os.path.join(temp_path, 'train')
+        temp_valid_path = os.path.join(temp_path, 'valid')
+
+        # Create the Image2Coco object
+        conveter = Image2Coco()
+
+        # Convert the mask to COCO format into a temporary folder
+        conveter.convert(data_train_path, temp_train_path, copy_images)
+        conveter.convert(data_valid_path, temp_valid_path, copy_images)
+
+        # Create the Coco2Yolo object
+        conveter = Coco2Yolo()
+
+        # Convert the COCO format to YOLO format from the temporary folder
+        conveter.convert(temp_train_path, yolo_train_path, copy_images)
+        conveter.convert(temp_valid_path, yolo_valid_path, copy_images)
+
+        # Create the data.yaml file
+        conveter.create_yaml(temp_train_path, yolo_path)
+
+        # Remove the temporary files
+        if os.path.exists(temp_path):
+            rmtree(temp_path)
+            
+if __name__ == "__main__":
     # Define the paths
-    data_train_path = 'dataset/train/'
-    data_valid_path = 'dataset/valid/'
+    data_path = 'dataset/'
     yolo_path = 'yolo/'
-    yolo_train_path = yolo_path + 'train/'
-    yolo_valid_path = yolo_path + 'valid/'
 
-    temp_path = yolo_path + 'temp/'
-    temp_train_path = temp_path + 'train/'
-    temp_valid_path = temp_path + 'valid/'
+    # Create the Image2Yolo object
+    conveter = Image2Yolo()
 
-    # Create the Image2Coco object
-    conveter = Image2Coco()
-
-    # Convert the mask to COCO format into a temporary folder
-    conveter.convert(data_train_path, temp_train_path, True)
-    conveter.convert(data_valid_path, temp_valid_path, True)
-
-    # Create the Coco2Yolo object
-    conveter = Coco2Yolo()
-
-    # Convert the COCO format to YOLO format from the temporary folder
-    conveter.convert(temp_train_path, yolo_train_path, True)
-    conveter.convert(temp_valid_path, yolo_valid_path, True)
-
-    # Create the data.yaml file
-    conveter.create_yaml(temp_train_path, yolo_path)
-
-    # Remove the temporary files
-    if os.path.exists(temp_path):
-        rmtree(temp_path)
+    # Convert the mask to Yolo format
+    conveter.convert(data_path, yolo_path, True)
